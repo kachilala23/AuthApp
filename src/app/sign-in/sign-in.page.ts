daddf7e40bc60150299153dc7c640238edfd235f
kachilala <kachilala23@gmail.com>
2022-01-17 13:57:59 +0100

    complete sign in page design with validation

--- src/app/sign-in/sign-in.page.ts
+++ src/app/sign-in/sign-in.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInPage implements OnInit {
 
+  form: FormGroup;
+  type = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      phone: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(10)]
+      }),
+      password: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(10)]
+      }),
+    });
+  }
+
+  changeType() {
+    this.type = !this.type;
+  }
+
+  signIn() {
+    if(!this.form.valid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    console.log(this.form.value);
   }
 
 }
